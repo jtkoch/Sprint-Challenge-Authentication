e58cbf6b4bf39c7a9a0d831cba0762bc059202a2
Jensen Koch <jtkoch12@gmail.com>
2020-05-17 20:14:00 -0500

    MVP

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,78 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
+//set up the Users object with the helper functions and the secret object
+const Users = require('../users/users-model');
+const secrets = require('../config/secret');
+
+//function to generate tokens for the client
+function generateToken(user) {
+  //the data
+  const payload = {
+    userId: user.id,
+    username: user.username,
+  };
+  const secret = secrets.jwtSecret;
+  const options = {
+    expiresIn: '1d',
+  };
+  return jwt.sign(payload, secret, options);
+}
+
+/** ENDPOINTS */
+
+//Register User
 router.post('/register', (req, res) => {
-  // implement registration
+  //set user to username and password via req.body
+  let user = req.body;
+
+  //set up the security hash rounds
+  const rounds = process.env.HASH_ROUNDS || 8;
+
+  //hash the password
+  const hash = bcrypt.hashSync(user.password, rounds);
+
+  //update the user to use the hash
+  user.password = hash;
+
+  //add the user
+  Users.add(user)
+    .then(saved => {
+      //if successful
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      //if failed to add to the server
+      res.status(500).json({ errorMessage: error.message });
+    });
 });
 
+//Login
 router.post('/login', (req, res) => {
-  // implement login
+  //set up the username and password objects to be checked against the login info
+  let { username, password } = req.body;
+
+  //search for the user using the username
+  Users.findBy({ username })
+    .then(([user]) => {
+      // if the username is found, check to see if the passwords match
+      if (user && bcrypt.compareSync(password, user.password)) {
+        //generate a token for the user
+        const token = generateToken(user);
+
+        //send the token to the client
+        res.status(200).json({ message: 'Successfully logged in!', token });
+      } else {
+        //if the password does not match
+        res.status(401).json({ message: 'Incorrect password.' });
+      }
+    })
+    //if the client cannot be found
+    .catch(err => {
+      res.status(500).json({ errorMessage: err.message });
+    });
 });
 
+//export
 module.exports = router;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,35 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+//set up the jwt object
+const jwt = require('jsonwebtoken');
 
+//set up secrets object
+const secrets = require('../config/secret.js');
+
+//export function that verifies the token for the client
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  //set up the token object based on the requested authorization
+  const token = req.headers.authorization.split(" ")[1];
+
+  //set secret to the jsonwebtoken secret
+  const secret = secrets.jwtSecret;
+
+  //verify the token
+  if (token) {
+    //if the token is valid
+    jwt.verify(token, secret, (error, decodedToken) => {
+      //if the token is valid, error will be undefined
+      if (error) {
+        res.status(401).json({ message: 'Invalid token' })
+      } else {
+        //set the requested token to the decodedtoken
+        req.decodedToken = decodedToken;
+        next();
+      }
+    });
+    //if there is an issue with the token before validation 
+  } else {
+    res.status(400).json({ errorMessage: 'Please provide credentials.' });
+  }
 };
